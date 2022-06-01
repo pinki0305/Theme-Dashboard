8f39f7466ae264315cdca69c5dd38d71a10ccc4f
pinki <pinki0305.niit@gmai.com>
2022-06-01 13:10:13 +0530

    Theme taken

--- src/App.js
+++ src/App.js
@@ -1,25 +1,271 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/style1.css";
+import "./css/boxicons.min.css";
 
 function App() {
+  function fun1() {
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function () {
+      sidebar.classList.toggle("active");
+      if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    };
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="sidebar">
+        <div class="logo-details">
+          <i class="bx bxl-c-plus-plus"></i>
+          <span class="logo_name">CodingLab</span>
+        </div>
+        <ul class="nav-links">
+          <li>
+            <a href="#" class="active">
+              <i class="bx bx-grid-alt"></i>
+              <span class="links_name">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-box"></i>
+              <span class="links_name">Product</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-list-ul"></i>
+              <span class="links_name">Order list</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-pie-chart-alt-2"></i>
+              <span class="links_name">Analytics</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-coin-stack"></i>
+              <span class="links_name">Stock</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-book-alt"></i>
+              <span class="links_name">Total order</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-user"></i>
+              <span class="links_name">Team</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-message"></i>
+              <span class="links_name">Messages</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-heart"></i>
+              <span class="links_name">Favrorites</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-cog"></i>
+              <span class="links_name">Setting</span>
+            </a>
+          </li>
+          <li class="log_out">
+            <a href="#">
+              <i class="bx bx-log-out"></i>
+              <span class="links_name">Log out</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <section class="home-section">
+        <nav>
+          <div class="sidebar-button">
+            <i class="bx bx-menu sidebarBtn" onClick={fun1}></i>
+            <span class="dashboard">Dashboard</span>
+          </div>
+          <div class="search-box">
+            <input type="text" placeholder="Search..."></input>
+            <i class="bx bx-search"></i>
+          </div>
+          <div class="profile-details">
+            <span class="admin_name">Prem Shahi</span>
+            <i class="bx bx-chevron-down"></i>
+          </div>
+        </nav>
+
+        <div class="home-content">
+          <div class="overview-boxes">
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Order</div>
+                <div class="number">40,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bx-cart-alt cart"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Sales</div>
+                <div class="number">38,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bxs-cart-add cart two"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Profit</div>
+                <div class="number">$12,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bx-cart cart three"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Return</div>
+                <div class="number">11,086</div>
+                <div class="indicator">
+                  <i class="bx bx-down-arrow-alt down"></i>
+                  <span class="text">Down From Today</span>
+                </div>
+              </div>
+              <i class="bx bxs-cart-download cart four"></i>
+            </div>
+          </div>
+
+          <div class="sales-boxes">
+            <div class="recent-sales box">
+              <div class="title">Recent Sales</div>
+              <div class="sales-details"></div>
+              <div class="button">
+                <a href="#">See All</a>
+              </div>
+            </div>
+            <div class="top-sales box">
+              <div class="title">Top Seling Product</div>
+              <ul class="top-sales-details">
+                <li>
+                  <a href="#">
+                    <span class="product">Vuitton Sunglasses</span>
+                  </a>
+                  <span class="price">$1107</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Hourglass Jeans </span>
+                  </a>
+                  <span class="price">$1567</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Nike Sport Shoe</span>
+                  </a>
+                  <span class="price">$1234</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Hermes Silk Scarves.</span>
+                  </a>
+                  <span class="price">$2312</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Succi Ladies Bag</span>
+                  </a>
+                  <span class="price">$1456</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Gucci Womens's Bags</span>
+                  </a>
+                  <span class="price">$2345</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Addidas Running Shoe</span>
+                  </a>
+                  <span class="price">$2345</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="product">Bilack Wear's Shirt</span>
+                  </a>
+                  <span class="price">$1245</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <br />
+          <br />
+          <br />
+          <div class="overview-boxes">
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Order</div>
+                <div class="number">40,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bx-cart-alt cart"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Sales</div>
+                <div class="number">38,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bxs-cart-add cart two"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Profit</div>
+                <div class="number">$12,876</div>
+                <div class="indicator">
+                  <i class="bx bx-up-arrow-alt"></i>
+                  <span class="text">Up from yesterday</span>
+                </div>
+              </div>
+              <i class="bx bx-cart cart three"></i>
+            </div>
+            <div class="box">
+              <div class="right-side">
+                <div class="box-topic">Total Return</div>
+                <div class="number">11,086</div>
+                <div class="indicator">
+                  <i class="bx bx-down-arrow-alt down"></i>
+                  <span class="text">Down From Today</span>
+                </div>
+              </div>
+              <i class="bx bxs-cart-download cart four"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
-
 export default App;
